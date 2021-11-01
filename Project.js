b971dd0360b602c46d6369f1983df843124c8c24
ashrafkhang8 <83918822+ashrafkhang8@users.noreply.github.com>
2021-11-01 20:06:33 +0530

    Update Project.js

--- Project.js
+++ Project.js
@@ -11,22 +11,35 @@ function sum() {
         r = rad,
         l = lan,
         b = bre;
-
-
+        
+        
+        
+        // Circle
     if (area == 'circle' || area == 'Circle') {
         document.getElementById('demo').innerHTML = `The area of ${area} :- ${PI*r**2 }cm`;
+        
+        
+        // rectangle
 
     } else if (area == 'rectangle' || area == 'Rectangle') {
         document.getElementById('demo').innerHTML = `The area of ${area} : ${l*b}`;
+        
+        // triangle 
 
     } else if (area == 'triangle' || area == 'Triangle') {
         document.getElementById('demo').innerHTML = `The area of ${area} : ${(1/2)*l*b}`;
-
+        
+        // square area 
     } else if(area == 'square' || area == 'Square'){
-        document.getElementById('demo').innerHTML = `The area of ${area} : ${l*l}`;
+        document.getElementById('demo').innerHTML = `The area of ${area} : ${l*l || b*b} `;
         
         
+        // Equilateral triangle
         
+    }else if(area == 'Equilateral triangle' || area == 'Equilateral' || area == 'equilateral triangle' || area=='Equilateral Triangle'){
+    document.getElementById('demo').innerHTML = `The area of ${area} : ${(1.73205081/4*l**2)} `;
+    
+    
     }else{
         console.log('false')
         document.getElementById('demo').innerHTML = `Not found fill Currect Values`;
